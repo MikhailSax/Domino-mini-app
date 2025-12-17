d654d24b1b3ba84f4fae7f85b1796c3c25f25efc
mv <dominobryanskiy@yandex.ru>
2025-12-17 14:26:49 +0800

    change with path for domain

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/REPO/", // <-- имя репозитория на GitHub
+  base: "/Domino-mini-app/"
 });
